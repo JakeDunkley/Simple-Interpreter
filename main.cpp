9ac8f00ffdcc97567321a0d00e53850513215b4e
jakeddunkley@gmail.com <Benny1032>
2022-02-18 17:32:04 -0500

    Program file characters can now be parsed, with some characters being ignored.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <queue>
 
 using namespace std;
 
@@ -28,13 +30,42 @@ struct Token {
 
 // Main.
 int main() {
+    // All characters that should be ignored.
+    const vector<char> ignoredChars = {'\n', ' ', '\t'};
 
-    // Open up streams for incoming program and output token file.
-    ifstream fileIn;
-    ofstream fileOut;
+    // Stream for incoming program file.
+    fstream testFile("/Users/jake/CLionProjects/Dunkley_CS4308_LexicalAnalyzer/program.txt");
 
-    fileIn.open("/Users/jake/CLionProjects/Dunkley_CS4308_LexicalAnalyzer/program.txt");
-    fileOut.open("/Users/jake/CLionProjects/Dunkley_CS4308_LexicalAnalyzer/tokens.txt");
+    // Holds raw character data from file.
+    vector<char> collectedChars;
+
+    // Holds finished token information for later use.
+    vector<Token> tokens;
+
+
+    if (testFile.is_open()) {
+        char curChar;                       // Holds value of current character being analyzed.
+        queue<char> literalIntegerRegister; // Holds values of integer being built.
+
+        // Main loop
+        while (testFile.get(curChar)) {
+            bool isValidChar = true;
+
+            for (char iC : ignoredChars) {
+                if (iC == curChar) isValidChar = false;
+            }
+
+            if (isValidChar) collectedChars.push_back(curChar);
+        }
+    }
+
+    else {
+        cout << "There was a problem opening the source file." << endl;
+    }
+
+    for (int i = 0; i < collectedChars.size(); i++) {
+        cout << i << ": \"" << collectedChars[i] << "\"" << endl;
+    }
 
     //TODO tokenize.
 
