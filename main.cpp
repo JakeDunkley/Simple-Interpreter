a24fb3ada1c9cbc88d0dc764c82ffd9c2bd8284d
jakeddunkley@gmail.com <Benny1032>
2022-03-06 21:43:39 -0500

    Finally works lol. Also refactored quite a bit.

--- main.cpp
+++ main.cpp
@@ -16,6 +16,11 @@ using namespace std;
 
 const bool debug = true;
 
+const string charsIgnore = "\n\t\r ";                       // All characters that should be ignored.
+const string charsLetters = "abcdefghijklmnopqrstuvwxyz";   // Characters that are valid for building an identifier.
+const string charsOperator = "=~<>*/-+()";                  // Characters that are valid for building an identifier.
+const string charsLiteralInteger = "0123456789";            // Characters that are valid for building an integer literal.
+
 // Used for labeling tokens that are later passed to the parser.
 typedef enum {
     badSymbol           = -1,
@@ -73,19 +78,6 @@ const map<string, TokenValue> keywords {
         {"print", keywordPrint}
 };
 
-// Easy way to find if a token lexeme is valid.
-TokenValue lookupToken(string& lexeme) {
-    if (operators.find(lexeme) != operators.end()) {
-        return operators.find(lexeme) -> second;
-    }
-
-    else if (keywords.find(lexeme) != keywords.end()) {
-        return keywords.find(lexeme) -> second;
-    }
-
-    else return badSymbol;
-}
-
 // Structure to hold token values.
 struct Token {
     TokenValue value;
@@ -101,45 +93,81 @@ struct Token {
     }
 };
 
+// Easy way to find if a token lexeme is valid.
+TokenValue lookupToken(string& lexeme) {
+    if (operators.find(lexeme) != operators.end()) {
+        return operators.find(lexeme) -> second;
+    }
+
+    else if (keywords.find(lexeme) != keywords.end()) {
+        return keywords.find(lexeme) -> second;
+    }
+
+    else return badSymbol;
+}
+
+Token* createNewToken(string& lexeme) {
+    return new Token{lookupToken(lexeme), lexeme};
+}
+
+Token* createNewToken(char& lexeme) {
+    string lexToString{string() + lexeme};
+    return new Token{lookupToken(lexToString), lexToString};
+}
+
+bool checkRegister(string& reg, vector<Token*>& toks) {
+    if (!reg.empty()) {
+        toks.push_back(createNewToken(reg));
+        cout << reg << endl;
+        reg.clear();
+        return true;
+    }
+
+    return false;
+}
+
+bool checkRegisterLetters(string& reg, vector<Token*>& toks) {
+    if (!reg.empty()) {
+        if (reg.size() == 1) {
+            toks.push_back(new Token(identifier, reg));
+        }
+
+        else {
+            toks.push_back(createNewToken(reg));
+        }
+
+        cout << reg << endl;
+        reg.clear();
+        return true;
+    }
+
+    return false;
+}
+
+// All data and variables needed for parsing.
+
+// Stream for incoming program file.
+// NOTE: macOS requires an absolute file path for some godforsaken reason.
+fstream testFile("/Users/jake/CLionProjects/Dunkley_CS4308_LexicalAnalyzer/program.txt");
+
+vector<Token*> tokens;          // Holds finished token information for later use.
+char charCurrent;               // Holds value of current character being analyzed.
+string registerLetters;         // Holds char(s) of keyword or identifier being built.
+string registerOperator;        // Holds chars of operator being built.
+string registerLiteralInteger;  // Holds chars of integer being built.
+
 // Main.
+// Abandon hope, all ye who enter...
 int main() {
-
-    // All characters that should be ignored.
-    const string charsIgnore = "\n\t\r ";
-
-    // Characters that are valid for building an integer literal.
-    const string charsLiteralInteger = "0123456789";
-
-    // Characters that are valid for building an identifier.
-    const string charsLetters = "abcdefghijklmnopqrstuvwxyz";
-
-    // Characters that are valid for building an identifier.
-    const string charsOperator = "=~<>*/-+()";
-
-    // Stream for incoming program file.
-    // NOTE: macOS requires an absolute file path for some godforsaken reason.
-    fstream testFile("/Users/jake/CLionProjects/Dunkley_CS4308_LexicalAnalyzer/program.txt");
-
-    // Holds finished token information for later use.
-    vector<Token*> tokens;
 
     // This area collects all non-ignored characters into a vector for easier parsing later.
     if (testFile.is_open()) {
 
-        char charCurrent;               // Holds value of current character being analyzed.
-        string registerLiteralInteger;  // Holds chars of integer being built.
-        string registerOperator;        // Holds chars of operator being built.
-        string registerLetters;         // Holds char(s) of keyword or identifier being built.
-
         // Loop for extracting characters out of program file.
         while (testFile.get(charCurrent)) {
 
-            if (debug) cout << "Current char:\t" << charCurrent << endl;
-
             // Check if the current character means there's a comment/EOL and ignore the comment/rest of the line.
             if (registerOperator == "/" && charCurrent == '/') {
-
-                if (debug) cout << charCurrent << ": Comment started" << endl;
 
                 while (charCurrent != '\n') {
                     testFile.get(charCurrent);
@@ -149,88 +177,47 @@ int main() {
                 continue;
             }
 
-            // Check if the current character is part of an integer literal.
-            if (charsLiteralInteger.find(charCurrent) != string::npos) {
-                if (debug) cout << charCurrent << ": is part of a number" << endl;
-                registerLiteralInteger.push_back(charCurrent);
-                continue;
+            if (charsIgnore.find(charCurrent) != string::npos) {
+                checkRegisterLetters(registerLetters, tokens);
+                checkRegister(registerOperator, tokens);
+                checkRegister(registerLiteralInteger, tokens);
             }
 
-            // If it isn't, see if an integer literal token can be created.
-            else if (!registerLiteralInteger.empty()) {
-                if (debug) cout << charCurrent << ": Creating integer" << endl;
-                tokens.push_back(new Token(literalInteger, registerLiteralInteger));
-                registerLiteralInteger.clear();
+            // Check if char is a letter.
+            else if (charsLetters.find(charCurrent) != string::npos) {
+                checkRegister(registerOperator, tokens);
+                checkRegister(registerLiteralInteger, tokens);
+
+                registerLetters.push_back(charCurrent);
             }
 
-            // Check if the current character is part of an operator.
-            if (charsOperator.find(charCurrent) != string::npos) {
+            // Check if char is an operator.
+            else if (charsOperator.find(charCurrent) != string::npos) {
+                checkRegisterLetters(registerLetters, tokens);
+                checkRegister(registerLiteralInteger, tokens);
 
-                if (debug) cout << charCurrent << ": is an operator" << endl;
-
-                // Check if the operator char is a paren.
-                // NOTE: Yes this is a very meh workaround, but it's the best I could
-                // come up with. There is probably a super elegant way to do this though...
                 if (charCurrent == '(' || charCurrent == ')') {
-                    if (debug) cout << charCurrent << ": is a paren" << endl;
-                    string paren{string() + charCurrent};
-                    tokens.push_back(new Token(operators.find(paren) -> second, paren));
-                    registerOperator.clear();
+                    tokens.push_back(createNewToken(charCurrent));
                 }
 
-                // If not a paren, add to register.
                 else {
-                    if (debug) cout << charCurrent << ": is NOT a paren" << endl;
                     registerOperator.push_back(charCurrent);
                 }
-
-                continue;
             }
 
-            // If it isn't, see if an operator token can be created.
-            else if (!registerOperator.empty()) {
-                if (debug) cout << charCurrent << ": Creating operator" << endl;
-                tokens.push_back(new Token(lookupToken(registerOperator), registerOperator));
-                registerOperator.clear();
-            }
+            // Check if char is an integer.
+            else if (charsLiteralInteger.find(charCurrent) != string::npos) {
+                checkRegisterLetters(registerLetters, tokens);
+                checkRegister(registerOperator, tokens);
 
-            // Check if the current character is part of a keyword or is an identifier.
-            if (charsLetters.find(charCurrent) != string::npos) {
-                if (debug) cout << charCurrent << ": is a letter" << endl;
-                registerLetters.push_back(charCurrent);
-                continue;
-            }
-
-            // If it isn't, see if a keyword token can be created.
-            else if (!registerLetters.empty()) {
-
-                if (debug) cout << charCurrent << ": Creating something" << endl;
-
-                // If it's one letter (i.e. an identifier), create a new identifier token.
-                if (registerLetters.size() == 1) {
-                    if (debug) cout << charCurrent << ": Creating identifier" << endl;
-                    tokens.push_back(new Token(identifier, string() + charCurrent));
-                }
-
-                // Otherwise, create a new keyword token.
-                else {
-                    if (debug) cout << charCurrent << ": Creating keyword" << endl;
-                    tokens.push_back(new Token(lookupToken(registerLetters), registerLetters));
-                }
-
-                registerLetters.clear();
-                continue;
-            }
-
-            // Check if the current character can be ignored.
-            if (charsIgnore.find(charCurrent) != string::npos) {
-                if (debug) cout << charCurrent << ": ignored" << endl;
-                continue;
+                registerLiteralInteger.push_back(charCurrent);
             }
 
             // Otherwise, create a bad symbol token.
-            if (debug) cout << charCurrent << ": is a bad symbol" << endl;
-            tokens.push_back(new Token(badSymbol, string() + charCurrent));
+            else {
+                string curToString{string() + charCurrent};
+                tokens.push_back(new Token(badSymbol, curToString));
+            }
 
         }
 
